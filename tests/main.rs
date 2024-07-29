cbf17bba365401bcf44c99fc215a095e32858152
nwrenger <nils@wrenger.net>
2024-07-30 00:44:34 +0200

    Revert ":sparkles: Added gluer crate and gluer-macro crate and moved functions accordingly"
    This reverts commit f905269d692d2e618b7161060458ed4579b9a57f.

--- tests/main.rs
+++ tests/main.rs
@@ -1,20 +1,15 @@
 use axum::{routing::get, Json, Router};
-use gluer::{add_route, cached, gen_ts};
-use hello::{Hello, CACHED_STRUCT_HELLO};
+use gluer::{add_route, api, cached};
 
 #[cached]
 async fn fetch_root() -> String {
     String::from("Hello, World!")
 }
 
-mod hello {
-    use gluer::cached;
-
-    #[cached]
-    #[derive(serde::Serialize, serde::Deserialize, Default)]
-    pub struct Hello {
-        name: String,
-    }
+#[cached]
+#[derive(serde::Serialize, serde::Deserialize, Default)]
+struct Hello {
+    name: String,
 }
 
 #[cached]
@@ -23,22 +18,16 @@ async fn add_root(Json(hello): Json<Hello>) -> Json<Hello> {
 }
 
 #[tokio::test]
+#[ignore = "everlasting server"]
 async fn main_test() {
     let mut app: Router<()> = Router::new();
-    let mut routes = vec![];
 
-    add_route!(routes, app, "/", get(fetch_root).post(add_root));
+    add_route!(app, "/", get(fetch_root).post(add_root));
 
-    gen_ts(
-        "tests/api.ts",
-        routes,
-        &[CACHED_FN_FETCH_ROOT, CACHED_FN_ADD_ROOT],
-        &[CACHED_STRUCT_HELLO],
-    )
-    .unwrap();
+    api!("tests/api.ts");
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
         .await
         .unwrap();
-    axum::serve(listener, app).await.unwrap(); // prevent everlasting server
+    axum::serve(listener, app).await.unwrap();
 }
